6042623de4d923921ffc83eb58597cc40b4cefd2
Jack Burgess <jackburgess124@gmail.com>
2021-04-21 17:33:19 +0100

    move efm config into lua, attempts to solve issues im having

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -31,8 +31,9 @@ local on_attach = function(client, bufnr)
 
   -- Linting
   if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<space>p", "<cmd>w<CR><cmd>lua vim.lsp.buf.formatting_sync({}, 1000)<CR><cmd>w<CR>", opts)
-    buf_set_keymap("n", "<space>P", "<cmd>w<CR><cmd>lua vim.lsp.buf.formatting()<CR><cmd>w<CR>", opts)
+    buf_set_keymap("n", "<space>p", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    buf_set_keymap("n", "<space>P", "<cmd>lua vim.lsp.buf.formatting_sync({}, 1000)<CR>", opts)
+    -- vim.cmd [[autocmd BufWritePre *.jsx,*.jsx lua vim.lsp.buf.formatting_sync(nil, 1500)]]
   end
   -- elseif client.resolved_capabilities.document_range_formatting then
     -- buf_set_keymap("n", "<space>p", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
@@ -72,12 +73,77 @@ for _, lsp in ipairs(servers) do
     }
   }
   elseif lsp == 'efm' then
+    local eslint = {
+      lintCommand = './node_modules/.bin/eslint -f unix --stdin --stdin-filename ${INPUT}',
+      lintIgnoreExitCode = true,
+      lintStdin = true,
+      lintFormats = { '%f:%l:%c: %m' }
+    }
+    local prettier = {
+      formatCommand = './node_modules/.bin/prettier --stdin-filepath ${INPUT}',
+      formatStdin = true
+    }
+    local prettierHtml = {
+      formatCommand = './node_modules/.bin/prettier --write ${--tab-width:tabWidth} ${--single-quote:singleQuote} --parser html --stdin-filepath ${INPUT}',
+      formatStdin = true
+    }
+    local prettierCss = {
+      formatCommand = './node_modules/.bin/prettier --write ${--tab-width:tabWidth} ${--single-quote:singleQuote} --parser css --stdin-filepath ${INPUT}',
+      formatStdin = true
+    }
+    local prettierJson = {
+      formatCommand = './node_modules/.bin/prettier --write ${--tab-width:tabWidth} ${--single-quote:singleQuote} --parser json --stdin-filepath ${INPUT}',
+      formatStdin = true
+    }
+    local pugLint = {
+      lintCommand = './node_modules/.bin/pug-lint --reporter inline',
+      lintIgnoreExitCode = true,
+      lintFormats = { '%f:%l:%c %m' },
+      rootMarkers = { '.pug-lintrc', '.pug-lintrc.js', '.pug-lintrc.json' }
+    }
+    local jsonLint = {
+      lintCommand = 'jq .',
+      lintStdin = true,
+      lintIgnoreExitCode = true,
+    }
+
+
     nvim_lsp[lsp].setup {
+      cmd = {
+        '/home/jack/.local/share/nvim/lspinstall/efm/efm-langserver'
+        , '-logfile', '/tmp/efm-lua.log', '-loglevel', '5'
+      },
       on_attach = on_attach,
       init_options = {
         documentFormatting = true
       },
-      settings = {}
+      settings = {
+        rootMarkers = { '.git/' },
+        lintDebounce = 1000000000, -- 1s in nanoseconds
+        languages = {
+          javascript = { eslint, prettier },
+          javascriptreact = { eslint, prettier },
+          typescript = { eslint, prettier },
+          typescriptreact = { eslint, prettier },
+          pug = { pugLint },
+          jade = { pugLint },
+          json = { jsonLint, prettierJson },
+          html = { prettierHtml },
+          css = { prettierCss },
+
+        }
+      },
+      filetypes = {
+        'javascriptreact',
+        'javascript',
+        'typescriptreact',
+        'typescript',
+        'pug',
+        'jade',
+        'json',
+        'css',
+        'html'
+      }
     }
   else
     nvim_lsp[lsp].setup { on_attach = on_attach }
